22ba1799b339c1f774a3627db1a41038c2c033b4
gpofcher <33699610+gpofcher@users.noreply.github.com>
2023-09-14 08:08:27 -0400

    Successfully called GCP Vision API

---
+++ example_google.py
@@ -0,0 +1,11 @@
+# basic usage of mtgscan with the Google Cloud Vision API
+
+from mtgscan.text import MagicRecognition
+from mtgscan.ocr.google_cloud import Google
+
+gcp = Google()
+rec = MagicRecognition(file_all_cards="all_cards.txt", file_keywords="Keywords.json")
+box_texts = gcp.image_to_box_texts("tests\samples\cube_RB\image.jpg")
+deck = rec.box_texts_to_deck(box_texts)
+for c, k in deck:
+    print(c, k)
--- mtgscan/ocr/google_cloud.py
+++ mtgscan/ocr/google_cloud.py
@@ -26,6 +26,32 @@ class Google(OCR):
 
         client = vision.ImageAnnotatorClient()
 
-        box_texts = BoxTextList()
+        # only local files for now
+        with open(image, "rb") as image_file:
+            content = image_file.read()
+
+        data = vision.Image(content=content)
+
+        logging.info(f"Send {image} to Google")
+        response = client.text_detection(image = data)
+        texts = response.text_annotations
+        print("Texts:")
+
+        for text in texts:
+            print(f'\n"{text.description}"')
+
+            vertices = [
+                f"({vertex.x},{vertex.y})" for vertex in text.bounding_poly.vertices
+            ]
+
+            print("bounds: {}".format(",".join(vertices)))
+
+        if response.error.message:
+            raise Exception(
+                "{}\nFor more info on error messages, check: "
+                "https://cloud.google.com/apis/design/errors".format(response.error.message)
+            )
+
+        # box_texts = BoxTextList()
         
-        return box_texts
+        # return box_texts
